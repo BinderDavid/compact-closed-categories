c6771df6f9460cb68d2639150150a0bf47880fce
BinderDavid <dbinder1989@googlemail.com>
2021-02-11 16:52:06 +0100

    Implement delta function

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE EmptyCase #-}
 
 module Lib where
 
@@ -55,5 +56,40 @@ data ProgramTerm (t :: ProgramType) where
   Dist :: ProgramTerm ((t1 :+: t2) :*: t3 :<->: (t1 :*: t3) :+: (t2 :*: t3))
   Factor :: ProgramTerm ((t1 :*: t3) :+: (t2 :*: t3) :<->: (t1 :+: t2) :*: t3)
 
+absurd :: ValueTerm Zero -> a
+absurd tm = case tm of {}
 
 deriving instance Show (ProgramTerm t)
+
+delta :: ProgramTerm (t1 :<->: t2) -> ValueTerm t1 -> ValueTerm t2
+-- Category
+delta Id _ = error "delta is not defined on Id"
+delta (Compose _ _) _ = error "delta is not defined on Compose"
+-- Congruence
+delta (CongSum _ _) _ = error "delta is not defined on CongSum"
+delta (CongProduct _ _) _ = error "delta is not defined on CongProduct"
+-- Sums
+delta UnitePlus (InjR tm) = tm
+delta UnitePlus (InjL tm) = absurd tm
+delta UnitiPlus tm = InjR tm
+delta SwapPlus (InjL tm) = InjR tm
+delta SwapPlus (InjR tm) = InjL tm
+delta AssocPlusL (InjL tm) = InjL (InjL tm)
+delta AssocPlusL (InjR (InjL tm)) = InjL (InjR tm)
+delta AssocPlusL (InjR (InjR tm)) = InjR tm
+delta AssocPlusR (InjL (InjL tm)) = InjL tm
+delta AssocPlusR (InjL (InjR tm)) = InjR (InjL tm)
+delta AssocPlusR (InjR tm) = InjR (InjR tm)
+-- Products
+delta UniteTimes (Pair TT tm) = tm
+delta UnitiTimes tm = Pair TT tm
+delta SwapTimes (Pair tm1 tm2) = Pair tm2 tm1
+delta AssocTimesL (Pair tm1 (Pair tm2 tm3)) = Pair (Pair tm1 tm2) tm3
+delta AssocTimesR (Pair (Pair tm1 tm2) tm3) = Pair tm1 (Pair tm2 tm3)
+-- Other
+delta AbsorbR (Pair tm _) = absurd tm
+delta Dist (Pair (InjL tm1) tm2) = InjL (Pair tm1 tm2)
+delta Dist (Pair (InjR tm1) tm2) = InjR (Pair tm1 tm2)
+delta Factor (InjL (Pair tm1 tm2)) = Pair (InjL tm1) tm2
+delta Factor (InjR (Pair tm1 tm2)) = Pair (InjR tm1) tm2
+
